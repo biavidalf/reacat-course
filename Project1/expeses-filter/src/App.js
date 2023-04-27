33ab1ebda910450309bf50ddc2967da8519a5806
Beatriz Vidal <74744158+biavidalf@users.noreply.github.com>
2023-04-27 15:55:22 -0300

    creating first project

---
+++ Project1/expeses-filter/src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="">
+      <h2>Lets get started</h2>
+      <ExpenseItem />
+      <ExpenseItem />
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
